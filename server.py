52be9a63db35a358b5363e7b90f9bd094cfdd940
yjoh <856251@naver.com>
2021-03-01 01:05:59 +0900

    server 수정

--- server.py
+++ server.py
@@ -1,36 +1,51 @@
 # TCP server example
 import socket
 import threading
-import json
+import time
 
-def echo_thread(client_socket, address):
-	print ("I got a connection from ", address)
-	while True:
-		data = client_socket.recv(512).decode('UTF-8')
-		if(data == 'q' or data == 'Q'):
-			client_socket.close()
-			break
+class Server:
+	def __init__(self, ip, port):
+		self.ip = ip
+		self.port = port
+		self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.soc.bind((self.ip, self.port))
+		self.soc.listen(2)
+		self.clients = []
+		self.loop = True
+
+	def start(self):
+		while self.loop:
+			client_sock, addr = self.soc.accept()
+			self.clients.append(client_sock)
+			t = threading.Thread(target=self.echo_thread, args=(client_sock, addr))
+			t.start()
+		
+		self.soc.close()
+
+
+	def echo_thread(self, client_socket, address):
+		print ("new connection : " + str(address[0]))
+
+		print(str(address[0]) + "//wating for other clients...")
+		while len(self.clients) < 2 and self.loop:
+			time.sleep(1000)
+
+		print(str(address[0]) + "//all clients connected")
+		if self.clients[0] == client_socket:
+			rival_sock = self.clients[1]
 		else:
-			print ("RECEIVED:" , data)
-			print (json.loads(data)['ball_data'])
-			break
-			#client_socket.send(data.encode())
+			rival_sock = self.clients[0]
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("172.16.100.74", 5000))
-server_socket.listen(5)
+		while self.loop:
+			data = client_socket.recv(512).decode()
+			print(str(address[0]) + "//client disconnected")
+			if(data == 'q' or data == 'Q'):
+				client_socket.close()
+				break
+			else:
+				print (str(address[0]) + "//RECEIVED:" , data)
+				rival_sock.send(data.encode())
 
-clients = []
-
-print ("TCPServer Start")
-print("host name : " + server_socket.getsockname()[0])
-
-while True:
-	t = threading.Thread(target=echo_thread, args=server_socket.accept())
-	t.start()
-
-	clients.append(t)
-
-server_socket.close()
-print("SOCKET closed... END")
-0
+serv = Server("", 5000)
+serv.start();
+serv.start();
